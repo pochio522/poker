94b59cfdf3261662fbfc4ba8bd28829f86d5d08d
pochio <yakata888@icloud.com>
2024-10-28 08:38:41 +0900

    数字の選択

--- app/multi/page.tsx
+++ app/multi/page.tsx
@@ -3,8 +3,12 @@ import React, { useState } from "react";
 
 const Page = () => {
   const [suit, setSuit] = useState(" ");
+  const [number, setNumber] = useState(" ");
   const handleSuitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSuit(event.target.value); // 選択されたスーツに更新
+  };
+  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(event.target.value); // 選択された数字に更新
   };
   return (
     <div className="container mx-auto p-4 flex flex-col items-center">
@@ -27,14 +31,40 @@ const Page = () => {
         <option value="♦"></option>
         <option value="♣"></option>
       </datalist>
+      {/* 数字選択用の入力フォーム */}
+      <label htmlFor="number" className="mb-4">
+        数字の選択:
+      </label>
+      <input
+        list="numbers" //どのデータリストを使うかid="numbers"のデータリストの使用
+        id="number"
+        name="number"
+        className="border-2 p-2 mb-5"
+        onChange={handleNumberChange}
+      />
+      <datalist id="numbers">
+        <option value="A"></option>
+        <option value="K"></option>
+        <option value="Q"></option>
+        <option value="J"></option>
+        <option value="T"></option>
+        <option value="9"></option>
+        <option value="8"></option>
+        <option value="7"></option>
+        <option value="6"></option>
+        <option value="5"></option>
+        <option value="4"></option>
+        <option value="3"></option>
+        <option value="2"></option>
+      </datalist>
       <div className="w-64 h-96 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex flex-col items-center justify-between p-4 relative">
         <div className="absolute top-2 left-2 flex flex-col items-center">
-          <span className="text-3xl font-bold">A</span>
+          <span className="text-3xl font-bold">{number}</span>
           <span className="text-3xl">{suit}</span>
         </div>
         <div className="text-9xl">{suit}</div>
         <div className="absolute bottom-2 right-2 flex flex-col items-center transform rotate-180">
-          <span className="text-3xl font-bold">A</span>
+          <span className="text-3xl font-bold">{number}</span>
           <span className="text-3xl">{suit}</span>
         </div>
       </div>
