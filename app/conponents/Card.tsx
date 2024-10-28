e80c127f1caa10129250e52b087d43abdb59ec7b
pochio <yakata888@icloud.com>
2024-10-28 21:57:34 +0900

    Cardコンポーネンの作成と置き換え

---
+++ app/conponents/Card.tsx
@@ -0,0 +1,77 @@
+//子コンポーネント(multi/page.tsxが親コンポーネント)
+// import React from "react";
+import React, { useState } from "react";
+// type Props = {
+//   suit: string;
+// };
+
+const Card = () => {
+  const [suit, setSuit] = useState(" ");
+  const [number, setNumber] = useState(" ");
+  const handleSuitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSuit(event.target.value); // 選択されたスーツに更新
+  };
+  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(event.target.value); // 選択された数字に更新
+  };
+  return (
+    <div className="flex flex-col items-center">
+      {/* スート選択用のプルダウン */}
+      <label htmlFor="suit" className="mb-4">
+        スートの選択:
+      </label>
+      <select
+        name="suit"
+        id="suit-select"
+        className="border-2 p-2 mb-5"
+        onChange={handleSuitChange}
+        value={suit}
+      >
+        <option value="">--スートの選択--</option>
+        <option value="♠">♠</option>
+        <option value="♥">♥</option>
+        <option value="♦">♦</option>
+        <option value="♣">♣</option>
+      </select>
+      {/* 数字選択用のプルダウン */}
+      <label htmlFor="number" className="mb-4">
+        数字の選択:
+      </label>
+      <select
+        name="number"
+        id="number-select"
+        className="border-4 p-2 mb-5"
+        onChange={handleNumberChange}
+        value={number}
+      >
+        {/*value=で numberに代入  その横で選択肢に表示される記号*/}
+        <option value="">--数字の選択--</option>
+        <option value="A">A</option> <option value="K">K</option>
+        <option value="Q">Q</option>
+        <option value="J">J</option>
+        <option value="T">T</option>
+        <option value="9">9</option>
+        <option value="8">8</option>
+        <option value="7">7</option>
+        <option value="6">6</option>
+        <option value="5">5</option>
+        <option value="4">4</option>
+        <option value="3">3</option>
+        <option value="2">2</option>
+      </select>
+      <div className="w-64 h-96 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex flex-col items-center justify-between p-4 relative">
+        <div className="absolute top-2 left-2 flex flex-col items-center">
+          <span className="text-3xl font-bold">{number}</span>
+          <span className="text-3xl">{suit}</span>
+        </div>
+        <div className="text-9xl">{suit}</div>
+        <div className="absolute bottom-2 right-2 flex flex-col items-center transform rotate-180">
+          <span className="text-3xl font-bold">{number}</span>
+          <span className="text-3xl">{suit}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- app/multi/page.tsx
+++ app/multi/page.tsx
@@ -1,75 +1,88 @@
 "use client"; //usestateを使うために必要
-import React, { useState } from "react";
+import React from "react";
+import Card from "../conponents/Card";
 
 const Page = () => {
-  const [suit, setSuit] = useState(" ");
-  const [number, setNumber] = useState(" ");
-  const handleSuitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSuit(event.target.value); // 選択されたスーツに更新
-  };
-  const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNumber(event.target.value); // 選択された数字に更新
-  };
   return (
     <div className="container mx-auto p-4 flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-8">トランプ</h1>
-
-      {/* スーツ選択用の入力フォーム */}
-      <label htmlFor="suit" className="mb-4">
-        スーツの選択:
-      </label>
-      <input
-        list="suits" //どのデータリストを使うかid="suits"のデータリストの使用
-        id="suit"
-        name="suit"
-        className="border-2 p-2 mb-5"
-        onChange={handleSuitChange}
-      />
-      <datalist id="suits">
-        <option value="♠"></option>
-        <option value="♥"></option>
-        <option value="♦"></option>
-        <option value="♣"></option>
-      </datalist>
-      {/* 数字選択用の入力フォーム */}
-      <label htmlFor="number" className="mb-4">
-        数字の選択:
-      </label>
-      <input
-        list="numbers" //どのデータリストを使うかid="numbers"のデータリストの使用
-        id="number"
-        name="number"
-        className="border-2 p-2 mb-5"
-        onChange={handleNumberChange}
-      />
-      <datalist id="numbers">
-        <option value="A"></option>
-        <option value="K"></option>
-        <option value="Q"></option>
-        <option value="J"></option>
-        <option value="T"></option>
-        <option value="9"></option>
-        <option value="8"></option>
-        <option value="7"></option>
-        <option value="6"></option>
-        <option value="5"></option>
-        <option value="4"></option>
-        <option value="3"></option>
-        <option value="2"></option>
-      </datalist>
-      <div className="w-64 h-96 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex flex-col items-center justify-between p-4 relative">
-        <div className="absolute top-2 left-2 flex flex-col items-center">
-          <span className="text-3xl font-bold">{number}</span>
-          <span className="text-3xl">{suit}</span>
-        </div>
-        <div className="text-9xl">{suit}</div>
-        <div className="absolute bottom-2 right-2 flex flex-col items-center transform rotate-180">
-          <span className="text-3xl font-bold">{number}</span>
-          <span className="text-3xl">{suit}</span>
-        </div>
-      </div>
+      <Card />
     </div>
   );
 };
 
 export default Page;
+// const Page = () => {
+//   // const [suit, setSuit] = useState(" ");
+//   // const [number, setNumber] = useState(" ");
+//   // const handleSuitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//   //   setSuit(event.target.value); // 選択されたスーツに更新
+//   // };
+//   // const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//   //   setNumber(event.target.value); // 選択された数字に更新
+//   // };
+//   return (
+//     <div className="container mx-auto p-4 flex flex-col items-center">
+//       <h1 className="text-3xl font-bold mb-8">トランプ</h1>
+
+//       <Card suit={suit} />
+
+//       {/* スーツ選択用の入力フォーム
+//       <label htmlFor="suit" className="mb-4">
+//         スーツの選択:
+//       </label>
+//       <input
+//         list="suits" //どのデータリストを使うかid="suits"のデータリストの使用
+//         id="suit"
+//         name="suit"
+//         className="border-2 p-2 mb-5"
+//         onChange={handleSuitChange}
+//       />
+//       <datalist id="suits">
+//         <option value="♠"></option>
+//         <option value="♥"></option>
+//         <option value="♦"></option>
+//         <option value="♣"></option>
+//       </datalist> */}
+//       {/* 数字選択用の入力フォーム */}
+//       {/* <label htmlFor="number" className="mb-4">
+//         数字の選択:
+//       </label>
+//       <input
+//         list="numbers" //どのデータリストを使うかid="numbers"のデータリストの使用
+//         id="number"
+//         name="number"
+//         className="border-2 p-2 mb-5"
+//         onChange={handleNumberChange}
+//       />
+//       <datalist id="numbers">
+//         <option value="A"></option>
+//         <option value="K"></option>
+//         <option value="Q"></option>
+//         <option value="J"></option>
+//         <option value="T"></option>
+//         <option value="9"></option>
+//         <option value="8"></option>
+//         <option value="7"></option>
+//         <option value="6"></option>
+//         <option value="5"></option>
+//         <option value="4"></option>
+//         <option value="3"></option>
+//         <option value="2"></option>
+//       </datalist>
+//       <div className="w-64 h-96 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex flex-col items-center justify-between p-4 relative">
+//         <div className="absolute top-2 left-2 flex flex-col items-center">
+//           <span className="text-3xl font-bold">{number}</span>
+//           <span className="text-3xl">{suit}</span>
+//         </div>
+//         <div className="text-9xl">{suit}</div>
+//         <div className="absolute bottom-2 right-2 flex flex-col items-center transform rotate-180">
+//           <span className="text-3xl font-bold">{number}</span>
+//           <span className="text-3xl">{suit}</span>
+//         </div>
+//       </div> */}
+//     </div>
+//   );
+// };
+
+// export default Page;
