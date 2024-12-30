8543e3878676a7c19b723eca59d62fc8c0a35c36
na2kera <nat3mitaro@gmail.com>
2024-12-31 00:35:36 +0900

    値を修正

--- app/conponents/Card.tsx
+++ app/conponents/Card.tsx
@@ -54,7 +54,7 @@ const Card = ({ suit, setSuit, number, setNumber, isUP }: CardProps) => {
         <option value="A">A</option> <option value="K">K</option>
         <option value="Q">Q</option>
         <option value="J">J</option>
-        <option value="T">T</option>
+        <option value="10">10</option>
         <option value="9">9</option>
         <option value="8">8</option>
         <option value="7">7</option>
