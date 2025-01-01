50862586b96dcccd9042490267f18b728e3da186
pochio <yakata888@icloud.com>
2025-01-01 14:08:43 +0900

    青と緑の部分の削除

--- app/page.tsx
+++ app/page.tsx
@@ -19,17 +19,16 @@ export default async function Home() {
 
   return (
     <div>
-      <div className="flex gap-4 mx-4">
-        <div className="w-1/2 flex flex-col gap-1">
-          <div className="bg-green-400">Column Left Row 1</div>
-          <div className="bg-green-400">Column Left Row 2</div>
-          <div className="bg-green-400">Column Left Row 3</div>
-        </div>
-        <div className="w-1/2  flex flex-col gap-1">
-          <div className="bg-blue-500">Column Right Row 1</div>
-          <div className="bg-blue-500">Column Right Row 2</div>
-          <div className="bg-blue-500">Column Right Row 3</div>
-        </div>
+      <div
+        style={{
+          //divで囲んだところをstyleで配置する
+          display: "flex",
+          justifyContent: "center",
+          // alignItems: "flex-start",
+          gap: "1rem",
+        }}
+      >
+        ここをクリックでmultiへ
       </div>
       <div
         style={{
